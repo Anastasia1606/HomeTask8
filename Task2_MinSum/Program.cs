e4e44db5c97ae3ff962754f6965ecd574ad3b333
Anastasia <nastya.arkanova.98@bk.ru>
2023-05-31 21:02:13 +0500

    add Task2_MinSum

---
+++ Task2_MinSum/Program.cs
@@ -0,0 +1,22 @@
+﻿int[,] arr = {
+  {5, 7, 1},
+  {3, 6, 9},
+  {2, 4 ,8}
+};
+int minSum = int.MaxValue;
+int minIndex = 0;
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+  int sum = 0;
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+    sum += arr[i, j];
+  }
+  if (sum < minSum)
+  {
+    minSum = sum;
+    minIndex = i;
+  }
+}
+Console.WriteLine($"Строка с наименьшей суммой элементов: {minIndex + 1}");
+Console.ReadLine();
