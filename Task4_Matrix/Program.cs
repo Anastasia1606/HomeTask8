8dcda55dbae7846b79a761c4ce9d172a2ece3023
Anastasia <nastya.arkanova.98@bk.ru>
2023-05-31 21:51:50 +0500

    add Task4_Matrix

---
+++ Task4_Matrix/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,,] arr = new int[2, 2, 2];
+Random rand = new Random();
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+    for (int k = 0; k < arr.GetLength(2); k++)
+    {
+      int num;
+      do
+      {
+        num = rand.Next(10, 100);
+      } 
+      while (Contains (arr, num));
+      arr[i, j, k] = num;
+    }
+  }
+} 
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+  for (int j = 0; j < arr.GetLength(1); j++)
+  {
+    for (int k = 0; k < arr.GetLength(2); k++)
+    {
+      Console.WriteLine($"arr[{i}, {j}, {k}] = {arr[i, j, k]}");
+    }
+  }
+}
+Console.ReadLine();
+static bool Contains(int[,,] arr, int num)
+{
+  foreach (int item in arr)
+  {
+    if (item == num)
+  {
+       return true;
+  }
+  }
+    return false;
+  }
